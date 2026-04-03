ef6f6661404d443922ffedf748cb3d1f590c90e4
core-e-40 <chl2099@g.rit.edu>
2026-04-03 01:49:30 -0400

    commit to test wagon

--- wagon/wagon.go
+++ wagon/wagon.go
@@ -15,8 +15,8 @@ func main() {
 
 	cmd := exec.Command("/bin/bash")
 	cmd.Stdin = bytes.NewReader(simple_bash_script)
-    // cmd.Stdout = os.Stdout // debugging
-    // cmd.Stderr = os.Stderr // debugging 
+    cmd.Stdout = os.Stdout // debugging
+    cmd.Stderr = os.Stderr // debugging 
     cmd.Run()
 	fmt.Println("Ran!")
 	// fmt.Println(cmd.Stdout)
