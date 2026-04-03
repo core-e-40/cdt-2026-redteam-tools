d06b74553759c3c3b301d3b1a1af696b967d87aa
core-e-40 <chl2099@g.rit.edu>
2026-04-03 01:47:53 -0400

    commit to test wagon

--- wagon/wagon.go
+++ wagon/wagon.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	_ "embed"
 	"fmt"
-	"os"
+	// "os"
 	"os/exec"
 )
 
@@ -15,11 +15,11 @@ func main() {
 
 	cmd := exec.Command("/bin/bash")
 	cmd.Stdin = bytes.NewReader(simple_bash_script)
-    cmd.Stdout = os.Stdout // debugging
-    cmd.Stderr = os.Stderr // debugging 
+    // cmd.Stdout = os.Stdout // debugging
+    // cmd.Stderr = os.Stderr // debugging 
     cmd.Run()
 	fmt.Println("Ran!")
-	fmt.Println(cmd.Stdout)
-	fmt.Println(cmd.Stderr)
+	// fmt.Println(cmd.Stdout)
+	// fmt.Println(cmd.Stderr)
 
 }
