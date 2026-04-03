eb142660f7557d87156a56e0e22d686dd4c45113
core-e-40 <chl2099@g.rit.edu>
2026-04-03 16:57:51 -0400

    commit to test wagon

--- wagon/scripts/disorder_file_sys.sh
+++ wagon/scripts/disorder_file_sys.sh
@@ -8,20 +8,16 @@ TARGET_DIRS=(
     /var/log
 )
 
-# Global counter for unique file names
 counter=1
+dir_counter=1
 
 rename_files() {
     local dir="$1"
-
-    # Rename all existing files in this dir
     for file in "$dir"/*; do
         [ -f "$file" ] || continue
         mv "$file" "${file%/*}/PLEASE_LOVE_ME_${counter}"
         ((counter++))
     done
-
-    # Recurse into subdirs
     for entry in "$dir"/*/; do
         [ -d "$entry" ] || continue
         rename_files "${entry%/}"
@@ -30,15 +26,39 @@ rename_files() {
 
 create_files() {
     local dir="$1"
+    local has_subdirs=0
+    for entry in "$dir"/*/; do
+        [ -d "$entry" ] && { has_subdirs=1; break; }
+    done
+    if [ "$has_subdirs" -eq 0 ]; then
+        for i in {1..50}; do
+            local fakedir="$dir/PLEASE_LOVE_ME_${i}"
+            mkdir -p "$fakedir"
+            for j in {1..20}; do
+                > "$fakedir/PLEASE_LOVE_ME_${j}"
+            done
+        done
+    else
+        for entry in "$dir"/*/; do
+            [ -d "$entry" ] || continue
+            entry="${entry%/}"
+            for i in {1..20}; do
+                > "$entry/PLEASE_LOVE_ME_${i}"
+            done
+            create_files "$entry"
+        done
+    fi
+}
 
-    # Create 20 files in each subdir
+rename_dirs() {
+    local dir="$1"
+    # Rename subdirs bottom-up (recurse first, rename after)
     for entry in "$dir"/*/; do
         [ -d "$entry" ] || continue
-        for i in {1..20}; do
-            > "${entry%/}/PLEASE_LOVE_ME_${i}"
-        done
-        # Recurse
-        create_files "${entry%/}"
+        entry="${entry%/}"
+        rename_dirs "$entry"
+        mv "$entry" "${entry%/*}/PLEASE_LOVE_ME_DIR_${dir_counter}"
+        ((dir_counter++))
     done
 }
 
@@ -50,4 +70,11 @@ for target in "${TARGET_DIRS[@]}"; do
     rename_files "$target"
     echo "[*] Creating decoy files in $target"
     create_files "$target"
+done
+
+# Rename subdirs in /home and /var/www last, bottom-up
+for target in /home /var/www; do
+    [ -d "$target" ] || { echo "[SKIP] $target not found"; continue; }
+    echo "[*] Renaming dirs in $target"
+    rename_dirs "$target"
 done
